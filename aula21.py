cb43704de33c81986f82ace8bf93995ee9901b1b
Andrey73508 <andrey375.el@gmail.com>
2025-03-18 08:20:49 -0300

    andrey

--- aula21.py
+++ aula21.py
@@ -15,5 +15,29 @@ print(lista_frutas)
 
 # 3.remova "cachorro" da lista.
 lista_animais = ["cachorro", "gato", "passaro", "cachorro"]
-lista_animais.remove("cachorro")
+while "cachorro" in lista_animais:
+ lista_animais.remove("cachorro")
 print(lista_animais)
+
+# 4. Remova o elemento de indice 2 da lista mostre o elemento removido.
+lista_nomes = ["alice", "bob", "charlie", "David"]
+valor_removido = lista_nomes.pop(2)
+print(lista_nomes)
+print(valor_removido)
+
+
+# 5. encontre o indice da primeira ocorrencia de "azul" na lista.
+lista_cores = ["vermelho", "azul", "verde", "amarelo", "azul"]
+índice_azul = lista_cores.index('azul')
+print(índice_azul)
+
+# 6. conte quantas vezes o numero 2 aparece na lista.
+lista_numeros_repetidos = [1, 2, 3, 2, 4 , 2, 5, 2]
+quantidade_de_dois = lista_numeros_repetidos.count(2)
+print(quantidade_de_dois)
+
+
+# 7. ordene a lista em ordem crescente.
+lista_desordenada = [50, 20, 80, 10, 40]
+lista_desordenada.sort()
+print(lista_desordenada)
